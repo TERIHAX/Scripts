9bee8ab673cc315fb730cac04d84e3ab9f8e8bdb
TERIZOROGAME <69073505+TERIHAX@users.noreply.github.com>
2021-04-14 16:52:13 +0100

    Update ZyrexHub.lua

--- ZyrexHub.lua
+++ ZyrexHub.lua
@@ -1,3 +1,3 @@
 _G.Toggle_GUI = Enum.KeyCode.RightControl -- Right Ctrl Keybind to Toggle GUI to Open or Close.
 
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/NotZyrex/Zyrex-Hub/master/Main.lua")))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NotZyrex/Zyrex-Hub/master/Main.lua"))()
