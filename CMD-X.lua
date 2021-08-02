f64f677ca986b598ca334d578a432399cf00a5ab
TERIZOROGAME <69073505+TERIHAX@users.noreply.github.com>
2021-08-02 17:38:33 +0100

    Update CMD-X.lua

--- CMD-X.lua
+++ CMD-X.lua
@@ -18,6 +18,6 @@
 |                | .cmds                             List commands |
 ----------------------------------------------------------------]]--
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source"))()
-
 warn("--------------------------------------------------------------------\n|                ▄▀▄▄▄▄   ▄▀▀▄ ▄▀▄  ▄▀▀█▄▄   ▄▀▀▄  ▄▀▄             |\n|               █ █    ▌ █  █ ▀  █ █ ▄▀   █ █    █   █             |\n|              ▐ █      ▐  █    █ ▐ █    █ ▐     ▀▄▀               |\n|                █        █    █    █    █      ▄▀ █               |\n|               ▄▀▄▄▄▄▀ ▄▀   ▄▀    ▄▀▄▄▄▄▀     █  ▄▀               |\n|              █     ▐  █    █    █     ▐    ▄▀  ▄▀                |\n|              ▐        ▐    ▐    ▐         █    ▐                 |\n|               ▐        ▐    ▐    ▐         █    ▐                |\n|------------------------------------------------------------------|\n|    Credits:    | Binds & Info:                                   |\n|    pigeon#1818 | U                         Open and close output |\n|        hz#4777 | RShift                          Fill suggestion |\n|     Curvn#2646 | ;                               Focus on CMDBar |\n| -------------- | Q                                Open and close |\n|                | LShift+Bksp                        Clear CMDbar |\n|                |                                                 |\n|                | .cmds                             List commands |\n--------------------------------------------------------------------")
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source"))()
