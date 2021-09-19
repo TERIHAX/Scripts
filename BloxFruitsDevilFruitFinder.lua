c5cf5f2b71b6361777480e4834e5b367f391bbbf
TERIZOROGAME <69073505+TERIHAX@users.noreply.github.com>
2021-09-19 16:35:59 +0100

    Create BloxFruitsDevilFruitFinder.lua

---
+++ BloxFruitsDevilFruitFinder.lua
@@ -0,0 +1,5 @@
+_G.Fruit = "YourFruitName Fruit" -- can be changed to the fruit you demand must be named like,"Spike Fruit" or "String Fruit"
+_G.EATFRUIT = true -- can be changed to false(NO CAPS) if you want the fruit to stay in its physical form also anti-afk script will activated and you will be teleported to a safe zone immediately
+_G.Team = 0 -- 0-1 you can choose to be Marines or Pirates it is defaulted to Marines
+
+loadstring(game:HttpGet('https://github.com/TERIHAX/Scripts/raw/main/Other/BloxFruitsFruitFinder.lua'))()
