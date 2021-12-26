06f5dcc72bf86bcba4a7d12acf641fd0249cb3c0
TERIZOROGAME <69073505+TERIHAX@users.noreply.github.com>
2021-12-26 19:18:33 +0000

    Update and rename BloxHub.lua to Script Hubs/BloxHub.lua

--- BloxHub.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/BloxiYT/BloxHub-v2/main/Main'))()
---
+++ Script Hubs/BloxHub.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet('https://github.com/BloxiYT/BloxHub-v2/raw/main/Main'))()
