df3d3fb5b26e58b9c072177a4f60730813cadd51
TERIZOROGAME <69073505+TERIHAX@users.noreply.github.com>
2021-12-26 20:37:31 +0000

    Update Main.lua

--- Other/SEHub/Main.lua
+++ Other/SEHub/Main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://github.com/TERIHAX/Scripts/raw/main/Other/SEHub/"..game.GameId..".lua"))()
+loadstring(game:HttpGet("https://github.com/TERIHAX/Scripts/raw/main/Other/SEHub/"..game.PlaceId..".lua"))()
