660a73e3e8da3c4e49ee8f8c36d4cd12b86d95e0
TERIZOROGAME <69073505+TERIHAX@users.noreply.github.com>
2022-09-11 19:18:07 +0100

    Create LazyHub.lua

---
+++ Script Hubs/LazyHub.lua
@@ -0,0 +1,4 @@
+-- IF THIS DOESN'T WORK USE THIS: loadstring(game:HttpGet('https://github.com/TERIHAX/Scripts/raw/main/Script%20Hubs/LazyHubBACKUP.lua'))()
+
+repeat wait() until game:IsLoaded()
+loadstring(game:HttpGet('https://github.com/LioK251/RbScripts/raw/main/lazyhub.lua'))()
