b0a338b4f54d3649580a4ebd6dea6254994e292a
TERIZOROGAME <69073505+TERIHAX@users.noreply.github.com>
2022-09-23 20:33:20 +0100

    Create PlaybackXHub.lua

---
+++ Script Hubs/PlaybackXHub.lua
@@ -0,0 +1,3 @@
+-- This script was obfuscated with MoonSec V2, so some exploits/executors might not be able to support it.
+
+loadstring(game:HttpGet("https://github.com/NeaPchX2/Playback-X-HUB/raw/main/Protected.lua.txt"))()
