75aea80d1b36f236908adfda214f5c7f1d7ed4ca
TERIZOROGAME <69073505+TERIHAX@users.noreply.github.com>
2022-10-01 15:06:45 +0100

    Create 6FootHub.lua

---
+++ Script Hubs/6FootHub.lua
@@ -0,0 +1,3 @@
+-- If it doesn't work, use this one instead: loadstring(game:HttpGet('https://github.com/TERIHAX/Scripts/raw/main/Script%20Hubs/6FootHubBACKUP.lua'))()
+
+loadstring(game:HttpGet('https://www.6footscripts.com/Scripts/6FootHub/Hub.lua'))()
