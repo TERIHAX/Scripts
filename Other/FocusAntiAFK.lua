69901afd9f05c729603bd126874216f73142bef4
TERI <69073505+TERIHAX@users.noreply.github.com>
2023-04-03 14:10:37 +0100

    Create FocusAntiAFK.lua

---
+++ Other/FocusAntiAFK.lua
@@ -0,0 +1,7 @@
+assert(firesignal, "Your exploit does not support firesignal.")
+local UserInputService: UserInputService = game:GetService("UserInputService")
+local RunService: RunService = game:GetService("RunService")
+UserInputService.WindowFocusReleased:Connect(function()
+   RunService.Stepped:Wait()
+   pcall(firesignal, UserInputService.WindowFocused)
+end)
