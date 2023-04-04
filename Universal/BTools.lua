f4fbd03a2941c4ec5ce480fa7d8aa001952aa2c8
TERI <69073505+TERIHAX@users.noreply.github.com>
2023-04-04 16:02:20 +0100

    Create BTools.lua

---
+++ Universal/BTools.lua
@@ -0,0 +1,16 @@
+backpack = game:GetService("Players").LocalPlayer.Backpack
+
+hammer = Instance.new("HopperBin")
+hammer.Name = "Hammer"
+hammer.BinType = 4
+hammer.Parent = backpack
+
+cloneTool = Instance.new("HopperBin")
+cloneTool.Name = "Clone"
+cloneTool.BinType = 3
+cloneTool.Parent = backpack
+
+grabTool = Instance.new("HopperBin")
+grabTool.Name = "Grab"
+grabTool.BinType = 2
+grabTool.Parent = backpack
