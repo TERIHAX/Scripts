375f6001453ab769e5ffe4b1e2796758900d6a82
TERI <69073505+TERIHAX@users.noreply.github.com>
2023-04-07 16:47:36 +0100

    Create SeleniumHub.lua

---
+++ Script Hubs/SeleniumHub.lua
@@ -0,0 +1,17 @@
+loadstring(game:HttpGet('https://githubusercontent.com/synnyyy/synergy/raw/main/selenium.lua', true))({
+	QueueOnTeleport = false -- This executes Selenium on server teleportation. If you want to enable this, set this to true. Otherwise set it to false.
+})
+
+-- Supported games are as follows:
+
+-- Rate my Avatar
+-- Pixel Piece
+-- Pilgrammed
+-- Arcane Odyssey
+-- My Restaurant
+
+---------------
+
+--[[
+    To get key, you gotta go in the discord, verify in the verification channel, then authenticate in that same channel. then type /key in the commands channel.
+]]--
