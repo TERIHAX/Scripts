2f6e63cadcd9d490babed28960959d21aaca3d08
TERI <69073505+TERIHAX@users.noreply.github.com>
2023-04-13 14:47:44 +0100

    Update Infinite Jump.lua

--- Universal/Infinite Jump.lua
+++ Universal/Infinite Jump.lua
@@ -1,9 +1,9 @@
 -- Make sure you do a getgenv here.
--- To disable inf jump, just change this ting below to false
-getgenv().InfiniteJump = true
+-- To disable inf jump, just re-execute/run this script.
+getgenv().infiniteJump = not getgenv().infiniteJump;
 
-game:GetService("UserInputService").JumpRequest:connect(function()
-    if InfiniteJump then
-		game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
+game:GetService('UserInputService').JumpRequest:connect(function()
+    if getgenv().infiniteJump then
+		game:GetService('Players').LocalPlayer.Character:FindFirstChildOfClass('Humanoid'):ChangeState('Jumping')
 	end
 end)
