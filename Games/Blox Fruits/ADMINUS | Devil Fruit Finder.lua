66254ee256166ee918a187605355154433de2117
TERI <69073505+TERIHAX@users.noreply.github.com>
2023-04-20 09:51:31 +0100

    Create ADMINUS | Devil Fruit Finder.lua

---
+++ Games/Blox Fruits/ADMINUS | Devil Fruit Finder.lua
@@ -0,0 +1,7 @@
+-- DO NOT CHOOSE A TEAM
+-- DO NOT CHOOSE A TEAM
+-- DO NOT CHOOSE A TEAM
+-- DO NOT CHOOSE A TEAM
+-- DO NOT CHOOSE A TEAM
+
+loadstring(game:HttpGet('https://scripts.luawl.com/15951/Adminus-BloxFruits.lua'))()
