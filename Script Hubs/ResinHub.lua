17ac232afb37c525fe44e01df156e50033ad3cec
TERI <69073505+TERIHAX@users.noreply.github.com>
2023-05-05 22:25:51 +0100

    Update ResinHub.lua

--- Script Hubs/ResinHub.lua
+++ Script Hubs/ResinHub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://github.com/sasaff94/Reskin-Hub/raw/main/README.md'))()
+loadstring(game:HttpGet('https://github.com/sasaff94/Reskin-Hub/raw/main/README.md', true))()
