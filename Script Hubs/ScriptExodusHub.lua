5c47e1299f159692d943c830e76a0354fb8cf547
TERI <69073505+TERIHAX@users.noreply.github.com>
2023-05-05 22:26:11 +0100

    Update ScriptExodusHub.lua

--- Script Hubs/ScriptExodusHub.lua
+++ Script Hubs/ScriptExodusHub.lua
@@ -1,2 +1,2 @@
 -- Script Exodus Hub, Put Together by TERI, Scripts Aren't Made by Me.
-loadstring(game:HttpGet("https://github.com/TERIHAX/Scripts/raw/main/Other/SEHub/"..game.PlaceId..".lua"))()
+loadstring(game:HttpGet('https://github.com/TERIHAX/Scripts/raw/main/Other/SEHub/' .. game.PlaceId .. '.lua', true))()
