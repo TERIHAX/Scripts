84de4b1ff7bc48c26e8c95acff370abd8b4ace11
TERI <69073505+TERIHAX@users.noreply.github.com>
2023-05-06 12:19:45 +0100

    Create Eduard'sHub (Fly, Walkspeed & More).lua

---
+++ Games/Da Hood/Eduard'sHub (Fly, Walkspeed & More).lua
@@ -0,0 +1,9 @@
+--[[
+Thanks For Using Our Hub
+Join Discord: https://discord.gg/jNm8rNTC2r
+]]--
+ 
+getgenv().PremiumKey = "" -- If you dont have premium leave the string empty or delete this line
+ 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LOLEduard/Eduard-s-Hub/main/script.lua"))()
+--SorryForNoUpdates
