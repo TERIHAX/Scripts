1a992c29cdedc8c8ab9dfed72019c3eef3119812
TERI <69073505+TERIHAX@users.noreply.github.com>
2023-05-08 13:35:14 +0100

    Create OrcaNightly.lua

---
+++ Script Hubs/OrcaNightly.lua
@@ -0,0 +1,3 @@
+-- this is a nightly/developer/snapshot build and can have bugs or cause issues or errors.
+
+loadstring(game:HttpGetAsync("https://github.com/richie0866/orca/raw/master/public/snapshot.lua"))()
