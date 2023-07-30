7be1674c51ac0a854e1c964d2d5b5868539a99e6
Enigmify <69073505+TERIHAX@users.noreply.github.com>
2023-07-30 16:32:34 +0100

    Create AdonisAntiCheatBypass.lua

---
+++ Other/AdonisAntiCheatBypass.lua
@@ -0,0 +1,3 @@
+-- backup: loadstring(game:HttpGet('https://github.com/TERIRepoBackups/releases/raw/main/bypass.lua', true))()
+
+loadstring(game:HttpGet('https://github.com/Tamim468/releases/raw/main/bypass.lua', true))()
