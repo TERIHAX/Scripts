d982b9766f927b3a1643e307ee448551af62bd35
Enigmify <69073505+TERIHAX@users.noreply.github.com>
2023-07-30 16:34:30 +0100

    Update JSONEncoderDecoder.lua

--- Other/JSONEncoderDecoder.lua
+++ Other/JSONEncoderDecoder.lua
@@ -1,1 +1,3 @@
+-- taken from valyse
+
 loadstring(game:HttpGet('https://github.com/Robbie-Wittenhagen/JSON/raw/Main/JSON.lua')..'\n\nhookfunction(game.HttpService.JSONEncode, JSON["encode"])\nhookfunction(game.HttpService.JSONDecode, JSON["decode"])')()
