efc0002a727893db50cac12f77a561f5dc8651dd
Enigmify <69073505+TERIHAX@users.noreply.github.com>
2023-07-30 16:34:12 +0100

    Create JSONEncoderDecoder.lua

---
+++ Other/JSONEncoderDecoder.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet('https://github.com/Robbie-Wittenhagen/JSON/raw/Main/JSON.lua')..'\n\nhookfunction(game.HttpService.JSONEncode, JSON["encode"])\nhookfunction(game.HttpService.JSONDecode, JSON["decode"])')()
